1c392988d4ef30ebb2c2d6b88d94574f834bb65a
StefanVanNijf <d276072@edu.rocwb.nl>
2024-02-21 12:30:18 +0100

    Revert "Revert "wip""
    This reverts commit 90a4eb0d750bd5ddbec18315db9e24583dbc726f.

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function orderedProducts()
+    {
+        return $this->hasMany(OrderedProduct::class);
+    }
+
+    public function monitorItems()
+    {
+        return $this->hasMany(MonitorItem::class);
+    }
+}
